bf2124b8f2f7c8b0ddbe03e7fd33d7b865bf76df
Ayesha Manzoor <131279891+Ayesha731@users.noreply.github.com>
2025-06-30 23:57:11 +0500

    commit5

--- Week4/chef_claude/chef_claude/src/component/main/Main.jsx
+++ Week4/chef_claude/chef_claude/src/component/main/Main.jsx
@@ -8,35 +8,47 @@ const Main = () => {
     <li key={ingredient}>{ingredient}</li>
   ));
 
+  // function handleSubmitted(formData) {
+  //   //1st method************************************
+  //   // event.preventDefault();
+  //   // console.log("The form is submitted!!");
+  //   // const formData = new FormData(event.currentTarget);
+  //   // const newIngredient = formData.get("ingredient");
+  //   // console.log(newIngredient);
+  //   // ingredient.push(newIngredient);
+  //   // console.log(ingredient);
+  //   // setIngredient((prevIngredient) => [
+  //   //   ...prevIngredient,
+  //   //   ingredient[prevIngredient.length],
+  //   // ]);
+  //   //2nd method********************************
+  //   // event.preventDefault();
+  //   // const formData = new FormData(event.currentTarget);
+  //   // const newIngredient = formData.get("ingredient");
+  //   // setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  //   //3rd method***************************************
+  //   const newIngredient = formData.get("ingredient"); //in get function the name of the input should be used
+  //   setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  //   console.log(newIngredient);
+  // }
   function handleSubmitted(event) {
-    // event.preventDefault();
-    // console.log("The form is submitted!!");
-    // const formData = new FormData(event.currentTarget);
-    // const newIngredient = formData.get("ingredient");
-    // console.log(newIngredient);
-    // ingredient.push(newIngredient);
-    // console.log(ingredient);
-    // setIngredient((prevIngredient) => [
-    //   ...prevIngredient,
-    //   ingredient[prevIngredient.length],
-    // ]);
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const newIngredient = formData.get("ingredient");
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+    event.target.reset(); // optional: clears input
   }
-
   return (
     <>
       <main>
-        <form className="add-ingredient-form" onSubmit={handleSubmitted}>
+        <form onSubmit={handleSubmitted} className="add-ingredient-form">
           <input
             type="text"
             placeholder="e.g. oregano"
             aria-label="Add ingredient"
             name="ingredient"
           />
-          <button>Add ingredient</button>
+          <button type="submit">Add ingredient</button>
         </form>
         <ul>{ingredientsListItems}</ul>
       </main>
