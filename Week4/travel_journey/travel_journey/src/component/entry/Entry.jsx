b50af3dc921cca11b4f11035f9c64defe30351c9
Ayesha <ayeshamanzoor731@gmail.com>
2025-07-12 01:04:03 +0500

    update

--- Week4/travel_journey/travel_journey/src/component/entry/Entry.jsx
+++ Week4/travel_journey/travel_journey/src/component/entry/Entry.jsx
@@ -8,17 +8,15 @@ const Entry = (props) => {
     <div>
       <article className="journel-entry">
         <div className="image-container">
-          <img
-            className="main-image"
-            src={props.img.src}
-            alt={props.img.alt}
-          />
+          <img className="main-image" src={props.img.src} alt={props.img.alt} />
         </div>
 
         <div className="info-container">
-          <FaLocationDot />
+          <FaLocationDot style={{ color: "red" }} />
           <span className="country">{props.country}</span>
-          <a href={props.googleMapsLink}>View on Google Maps</a>
+          <a href={props.googleMapsLink} style={{ color: "blue" }}>
+            View on Google Maps
+          </a>
           <h2 className="entry-title">{props.title}</h2>
           <p className="trip-dates">{props.dates}</p>
           <p className="entry-text">{props.text}</p>
